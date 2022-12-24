06e56cc71dbcb3e4fbe047a68d20c40af8da371e
Arthur <artursharipv@mail.ru>
2022-12-24 10:24:26 +0500

    correct quick sort, now center elem is random

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -1,9 +1,11 @@
+import random
 def quick_sort(a):
     if len(a) <= 1:
         return a
-    center = [i for i in a if i == a[0]]
-    left = list(filter(lambda x: x<a[0], a))
-    right = list(filter(lambda x: x>a[0], a))
+    elem = random.choice(a)
+    center = [i for i in a if i == elem]
+    left = list(filter(lambda x: x<elem, a))
+    right = list(filter(lambda x: x>elem, a))
     return quick_sort(left) + center + quick_sort(right)
 
 a = [9, 5, 7, -5, 0, 0, 1]
